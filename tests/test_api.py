fb8c49c2fd5300c00e69158bc393d8a41f7e7a9d
nkr <roba2005avagyan@gmail.com>
2025-05-22 11:17:37 +0300

    Настроен GitHub Actions для автотестов

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,6 @@ import requests
 
 
 def test_login_success():
-    response = requests.post("http://127.0.0.1:8080/?")
+    response = requests.get("http://127.0.0.1:8080/?")
     assert response.status_code == 200
     assert "token" in response.json()
